24f4652399f07ce7021751ee956fb13f9eb8eebc
Michael Shiferaw <michaelshiferaw@Mac.lan>
2025-05-05 11:53:59 -0400

    1st task or mini project

---
+++ accordion/src/components/accordian/data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        id: "1",
+        question: "what is an accordion component?",
+        answer : "accordion components are user interface components"
+    },
+    {
+        id: "2",
+        question: "what are they used for?",
+        answer: "they are used components are user interface components"
+    },
+    {
+        id: "3",
+        question: "accordion is a musical instrumet?",
+        answer: "accordion components are user interface components"
+    }
+    
+];
+
+export default data
